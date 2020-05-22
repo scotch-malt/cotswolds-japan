3a4d2f1f247af75cadf2285cafc0093567eaed43
William Ward <william-w@sms.local>
2020-05-22 17:54:54 +0900

    Added social icons

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,34 @@
 import React from "react";
 import Layout from "../components/layout";
+import Footer from "../components/footer";
+import Facebook from "../images/facebook.svg";
+import Instagram from "../images/instagram.svg";
+import Twitter from "../images/twitter.svg";
+
+import "../styles/contact.scss";
 
 const Contact = () => {
     return (
         <Layout>
             <div className="contact">
-                <h1 style={{padding: "100px 0"}}>This is the Contact page.</h1>
+                <div className="contact-main-content">
+                    <h1>CONTACT US</h1>
+                    <div className="contact-main-content-social">
+                        <img src={Facebook} alt="Facebook" className="img-fluid" title="Facebook"/>
+                        <img src={Instagram} alt="Instagram" className="img-fluid" title="Instagram"/>
+                        <img src={Twitter} alt="Twitter" className="img-fluid" title="Twitter"/>
+                    </div>
+                    <form className="contact-main-content-form">
+                        <h3 className="text-center py-4">General Inquiries</h3>
+                        <div className="contact-main-content-form-group">
+                            <input type="text"/>
+                            <input type="text"/>
+                            <input type="text"/>
+                        </div>
+                        
+                    </form>
+                </div>
+                <Footer />
             </div>
         </Layout>
         
